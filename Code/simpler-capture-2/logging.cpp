a14486e4dba8c9ae9fc966bb970f84c00bff0ad9
ORFMark <markrb0609@gmail.com>
2020-11-11 13:55:25 -0700

    split out some logging code

---
+++ Code/simpler-capture-2/logging.cpp
@@ -0,0 +1,18 @@
+#include "logging.h"
+#include <syslog.h>
+
+void log(string thingToLog) {
+   log(thingToLog, LOG_INFO);
+}
+
+void log(string thingToLog, int logLevel) {
+    struct timeval tv;
+   gettimeofday(&tv, (struct timezone *) 0);
+   if(USE_PRINTF) {
+     cout << PROJECT_TAG << ": SEC:USEC; " << tv.tv_sec << ":" << tv.tv_usec 
+        << "; " << thingToLog << endl;
+   }
+     syslog(logLevel, "%s; SEC:USEC; %d:%d; %s", PROJECT_TAG
+        , tv.tv_sec, tv.tv_usec, thingToLog);
+   
+}
