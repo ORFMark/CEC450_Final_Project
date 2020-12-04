acffe1e68275d327962d80457390b31d9d98151d
Mark Burrell <markrb0609@gmail.com>
2020-12-04 14:34:14 -0800

    Formatting cleanup

--- Code/simpler-capture-2/logging.cpp
+++ Code/simpler-capture-2/logging.cpp
@@ -1,33 +1,29 @@
-
 // Global include section
 #include <syslog.h>
 #include <sys/time.h>
 #include <iostream>
 
-
 // Local include section
 #include "logging.h"
-
 
 // 
 using namespace std;
 struct timeval tv;
 
-
 // Simple override function for logging at LOG_INFO level
-void log(char * thingToLog) {
-   log(thingToLog, LOG_INFO);
+void log(char *thingToLog) {
+	log(thingToLog, LOG_INFO);
 }
 
-
 // Function for logging the wanted text at the wanted logging level
-void log(char * thingToLog, int logLevel) {
-   gettimeofday(&tv, (struct timezone *) 0);
+void log(char *thingToLog, int logLevel) {
+	gettimeofday(&tv, (struct timezone*) 0);
 
-   if(USE_PRINTF) {
-     cout << PROJECT_TAG << ": SEC:USEC; " << tv.tv_sec << ":" << tv.tv_usec 
-        << "; " << thingToLog << endl;
-   }
-   
-   syslog(logLevel, "%s; SEC:USEC; %lu:%lu; %s", PROJECT_TAG, tv.tv_sec, tv.tv_usec, thingToLog);
+	if (USE_PRINTF) {
+		cout << PROJECT_TAG << ": SEC:USEC; " << tv.tv_sec << ":" << tv.tv_usec
+				<< "; " << thingToLog << endl;
+	}
+
+	syslog(logLevel, "%s; SEC:USEC; %lu:%lu; %s", PROJECT_TAG, tv.tv_sec,
+			tv.tv_usec, thingToLog);
 }
--- Code/simpler-capture-2/logging.h
+++ Code/simpler-capture-2/logging.h
@@ -1,18 +1,16 @@
-
 #ifndef LOGGING_BG
 #define LOGGING_BG
-
 
 // Defines section
 #define PROJECT_TAG "BGRTR_RT_Final"
 
-
 // External variables section
 bool USE_PRINTF;
 
-
 // Functions for logging specific information
-void log(char * thingToLog, int logLevel);
-void log(char * thingToLog);
-void setPrintf(bool usePrintf) { USE_PRINTF = usePrintf;}
+void log(char *thingToLog, int logLevel);
+void log(char *thingToLog);
+void setPrintf(bool usePrintf) {
+	USE_PRINTF = usePrintf;
+}
 #endif // LOGGING_BG
