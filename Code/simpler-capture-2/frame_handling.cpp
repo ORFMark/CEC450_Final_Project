2e0631035258e657eefa3beece40f0691501dfca
rappt <44380397+rappt@users.noreply.github.com>
2020-12-04 22:47:29 -0700

    Revert "Formatting cleanup"
    This reverts commit acffe1e68275d327962d80457390b31d9d98151d.

--- Code/simpler-capture-2/frame_handling.cpp
+++ Code/simpler-capture-2/frame_handling.cpp
@@ -1,8 +1,10 @@
+
 // Global include section
 #include <stdlib.h>
 #include <iostream>
 //#include <string>
 #include <sys/time.h>
+
 
 // OpenCV include section
 #include <opencv2/core/core.hpp>
@@ -10,164 +12,172 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+
 // Local include section
-#include "frame_handling.h"
-#include "util.h"
+#include "frame_handling.hpp"
+
+
 // 
 using namespace std;
 using namespace cv;
 
+
 // 
 Frame localFramePointer;
 
+
 // 
-bool initQueue(FrameQueue *frameQueueToInit, int size) {
-#ifdef EMPTY_POTENTIAL_QUEUE
-    if (frameQueueToInit != NULL) {
-        delete frameQueueToInit;
-        frameQueueToInit = NULL;
+bool initQueue(FrameQueue * WantedFrameQueue, int size) {
+    #ifdef EMPTY_POTENTIAL_QUEUE
+    if (WantedFrameQueue != NULL) {
+        delete WantedFrameQueue;
+        WantedFrameQueue = NULL;
     }
-    if (frameQueueToInit == NULL) frameQueueToInit = (FrameQueue*)malloc(sizeof(FrameQueue));
+    if (WantedFrameQueue == NULL) WantedFrameQueue = (FrameQueue*)malloc(sizeof(FrameQueue));
     #endif
-	frameQueueToInit->frames = (Frame**) malloc(size * sizeof(Frame*));
-	frameQueueToInit->frames_array = (Frame*) malloc(size * sizeof(Frame));
-	frameQueueToInit->nextFrameIndex = 0;
-	frameQueueToInit->numberOfFrames = 0;
-	frameQueueToInit->starterIndex = 0;
-	if (frameQueueToInit->frames != NULL
-			&& frameQueueToInit->frames_array != NULL) {
-		frameQueueToInit->maxSize = size;
-	} else {
-		frameQueueToInit->maxSize = 0;
-		return false;
-	}
-	return true;
+    WantedFrameQueue->frames = (Frame**)malloc(size * sizeof(Frame*));
+    WantedFrameQueue->frames_array = (Frame*)malloc(size * sizeof(Frame));
+    WantedFrameQueue->nextFrameIndex = 0;
+    WantedFrameQueue->numberOfFrames = 0;
+    WantedFrameQueue->starterIndex = 0;
+    if(WantedFrameQueue->frames != NULL && WantedFrameQueue->frames_array != NULL) {
+      WantedFrameQueue->maxSize = size;
+    } else {
+       WantedFrameQueue->maxSize = 0;
+       return false;
+    }
+    return true;
 }
 
-//
-bool destructQueue(FrameQueue *WantedFrameQueue) {
-	free(WantedFrameQueue->frames);
-	free(WantedFrameQueue->frames_array);
-}
 
 // 
-bool isEmpty(FrameQueue *queue) {
-	return (queue->numberOfFrames <= 0);
+bool destructQueue(FrameQueue * WantedFrameQueue) {
+    free(WantedFrameQueue->frames);
+    free(WantedFrameQueue->frames_array);
 }
+
 
 // 
-bool IsFull(FrameQueue *InputtedQueue) {
-	return (InputtedQueue->numberOfFrames >= InputtedQueue->maxSize);
+bool isEmpty(FrameQueue * queue) {
+   return (queue->numberOfFrames <= 0);
 }
+
 
 // 
-bool enqueue(FrameQueue *WantedQueue, Frame *WantedFrame) {
-	if (WantedQueue->numberOfFrames < WantedQueue->maxSize) {
-		WantedQueue->frames[WantedQueue->nextFrameIndex] = WantedFrame;
-		WantedQueue->frames_array[WantedQueue->nextFrameIndex].frame =
-				WantedFrame->frame;
-		WantedQueue->frames_array[WantedQueue->nextFrameIndex].capture_timestamp =
-				WantedFrame->capture_timestamp;
-		WantedQueue->frames_array[WantedQueue->nextFrameIndex].capture_timestamp_int =
-				WantedFrame->capture_timestamp_int;
-		++(WantedQueue->numberOfFrames);
-		if (WantedQueue->nextFrameIndex >= (WantedQueue->maxSize - 1)) {
-			WantedQueue->nextFrameIndex = 0;
-		} else {
-			++(WantedQueue->nextFrameIndex);
-		}
-		return true;
-	} else {
-		return false;
-	}
-	/*
-	 if(queue->numberOfFrames < queue->maxSize) {
-	 queue->frames[queue->nextFrameIndex] = frame;
-	 queue->nextFrameIndex = queue->nextFrameIndex + 1 % queue->maxSize;
-	 queue->numberOfFrames++;
-	 return true;
-	 }
-	 return false;*/
+bool IsFull(FrameQueue * InputtedQueue) {
+    return (InputtedQueue->numberOfFrames >= InputtedQueue->maxSize);
 }
+
 
 // 
-bool dequeue(FrameQueue *WantedQueue, Frame *DequeuedFrame) {
-
-	if (WantedQueue->numberOfFrames > 0) {
-		DequeuedFrame = queue->frames[WantedQueue->starterIndex];
-		//(queue->frames[WantedQueue->starterIndex]) = NULL;
-		++(WantedQueue->starterIndex);
-		--(WantedQueue->numberOfFrames);
-		return true;
-	}
-	return false;
-
-	/*
-	 Frame* frame = NULL;
-	 if(queue->numberOfFrames < queue->maxSize) {
-	 if(queue->nextFrameIndex != 0) {
-	 frame = queue->frames[--queue->nextFrameIndex];
-	 } else {
-	 frame = queue->frames[queue->maxSize - 1];
-	 queue->nextFrameIndex = queue->maxSize - 1;
-	 }
-	 queue->nextFrameIndex--;
-	 }
-	 return *frame;*/
+bool enqueue(FrameQueue * WantedQueue, Frame * WantedFrame) {
+    if(WantedQueue->numberOfFrames < WantedQueue->maxSize) {
+        WantedQueue->frames[WantedQueue->nextFrameIndex] = WantedFrame;
+        WantedQueue->frames_array[WantedQueue->nextFrameIndex].frame = WantedFrame->frame;
+        WantedQueue->frames_array[WantedQueue->nextFrameIndex].capture_timestamp = WantedFrame->capture_timestamp;
+        WantedQueue->frames_array[WantedQueue->nextFrameIndex].capture_timestamp_int = WantedFrame->capture_timestamp_int;
+        ++(WantedQueue->numberOfFrames);
+        if (WantedQueue->nextFrameIndex >= (WantedQueue->maxSize - 1)) {
+            WantedQueue->nextFrameIndex = 0;
+        } else {
+            ++(WantedQueue->nextFrameIndex);
+        }
+        return true;
+     } else {
+        return false;
+     }
+    /*
+    if(queue->numberOfFrames < queue->maxSize) {
+        queue->frames[queue->nextFrameIndex] = frame;
+        queue->nextFrameIndex = queue->nextFrameIndex + 1 % queue->maxSize;
+        queue->numberOfFrames++;
+        return true;
+     }
+     return false;*/
 }
 
-//
-void captureFrame(CvCapture *camToCaptureFrom, Frame *FramePlacement) {
-	FramePlacement.frame = cvQueryFrame(camToCaptureFrom);
-	FramePlacement.capture_timestamp = getTimeMsec(
-			FramePlacement.capture_timestamp_int);
 
-	/*
-	 IplImage* img = cvQueryFrame(camToCaptureFrom);
-	 double time = getTimeMsec();
-	 Frame frame;
-	 frame.frame = img;
-	 frame.capture_timestamp = time;
-	 return frame;*/
+// 
+bool dequeue(FrameQueue * WantedQueue, Frame * DequeuedFrame) {
+    
+    if (WantedQueue->numberOfFrames > 0) {
+        DequeuedFrame = queue->frames[WantedQueue->starterIndex];
+        //(queue->frames[WantedQueue->starterIndex]) = NULL;
+        ++(WantedQueue->starterIndex);
+        --(WantedQueue->numberOfFrames);
+        return true;
+    }
+    return false;
+    
+    /*
+    Frame* frame = NULL;
+    if(queue->numberOfFrames < queue->maxSize) {
+        if(queue->nextFrameIndex != 0) { 
+            frame = queue->frames[--queue->nextFrameIndex];
+        } else {
+            frame = queue->frames[queue->maxSize - 1];
+            queue->nextFrameIndex = queue->maxSize - 1;
+        }
+        queue->nextFrameIndex--;
+    }
+    return *frame;*/
 }
+
+
+// 
+void captureFrame(CvCapture * camToCaptureFrom, Frame * FramePlacement) {
+    FramePlacement.frame = cvQueryFrame(camToCaptureFrom);
+    FramePlacement.capture_timestamp = getTimeMsec(FramePlacement.capture_timestamp_int);
+    
+    /*
+    IplImage* img = cvQueryFrame(camToCaptureFrom);
+    double time = getTimeMsec();
+    Frame frame;
+    frame.frame = img;
+    frame.capture_timestamp = time;
+    return frame;*/
+}
+
 
 // SHOULD NOT BE UTILIZING STRINGS, AS THEY RESULT IN DYNAMIC MEMORY ALLOCATIONS
-void writebackFrame(int frameNum, Frame *frame) {
-	static Mat img; // Need to get rid of this
-	//double msecTime = frame.capture_timestamp;
-#ifdef NULLPTR_CHECKS
+void writebackFrame(int frameNum, Frame * frame) {
+    static Mat img; // Need to get rid of this
+    //double msecTime = frame.capture_timestamp;
+    #ifdef NULLPTR_CHECKS
     if (frame != NULL) {
     #else
-#warning "Should provide -DNULLPTR_CHECKS while compiling this file to minimize potential errors."
-#endif
-	img = cvarrToMat(frame->frame);
-#ifdef NULLPTR_CHECKS
+    #warning "Should provide -DNULLPTR_CHECKS while compiling this file to minimize potential errors."
+    #endif
+    img = cvarrToMat(frame->frame);
+    #ifdef NULLPTR_CHECKS
     }
     #endif
-	/* Why are we adding a string onto the image?
-	 String headerString = "Frame: " + to_string(frameNum) + "   msecTime: " + to_string(msecTime);
-	 putText(img, headerString, cv::Point2f(100,100), cv::FONT_HERSHEY_PLAIN
-	 , 2, cv::Scalar(0,0,255,255));*/
-	static char LocalFileName[24] = "frames/frame_******.ppm";
-	snprintf(LocalFileName, 24, "frames/frame_%06d.ppm", frameNum);
-	//String fileName = "frames/frame_" + to_string(frameNum) + ".ppm";
-	imwrite(LocalFileName/*fileName.c_str()*/, img);
+    /* Why are we adding a string onto the image?
+    String headerString = "Frame: " + to_string(frameNum) + "   msecTime: " + to_string(msecTime);
+    putText(img, headerString, cv::Point2f(100,100), cv::FONT_HERSHEY_PLAIN
+        , 2, cv::Scalar(0,0,255,255));*/
+    static char LocalFileName[24] = "frames/frame_******.ppm";
+    snprintf(LocalFileName, 24, "frames/frame_%06d.ppm", frameNum);
+    //String fileName = "frames/frame_" + to_string(frameNum) + ".ppm";
+    imwrite(LocalFileName/*fileName.c_str()*/, img);
 }
 
-// 
-void writeBackFrameService(FrameQueue *frameQueue) {
-	static int frameNumber = 0;
-	while (!isEmpty(frameQueue)) {
-		if (dequeue(frameQueue, &localFramePointer) == true) {
-			writebackFrame(frameNumber, &localFramePointer);
-		}
-		++frameNumber;
-	}
-	frameNumber = 0;
-}
 
 // 
-void captureFrameService(CvCapture *camToCaptureFrom, FrameQueue *frameQueue) {
-	captureFrame(camToCaptureFrom, &localFramePointer);
-	enqueue(frameQueue, &localFramePointer);
+void writeBackFrameService(FrameQueue * frameQueue) {
+    static int frameNumber = 0;
+    while(!isEmpty(frameQueue)) {
+        if (dequeue(frameQueue, &localFramePointer) == true) {
+            writebackFrame(frameNumber, &localFramePointer);
+        }
+        ++frameNumber;
+    }
+    frameNumber = 0;
+}
+
+
+// 
+void captureFrameService(CvCapture * camToCaptureFrom, FrameQueue * frameQueue) {
+    captureFrame(camToCaptureFrom, &localFramePointer);
+    enqueue(frameQueue, &localFramePointer);
 }
--- Code/simpler-capture-2/logging.h
+++ Code/simpler-capture-2/logging.h
@@ -1,16 +1,18 @@
+
 #ifndef LOGGING_BG
 #define LOGGING_BG
+
 
 // Defines section
 #define PROJECT_TAG "BGRTR_RT_Final"
 
+
 // External variables section
 bool USE_PRINTF;
 
+
 // Functions for logging specific information
-void log(char *thingToLog, int logLevel);
-void log(char *thingToLog);
-void setPrintf(bool usePrintf) {
-	USE_PRINTF = usePrintf;
-}
+void log(char * thingToLog, int logLevel);
+void log(char * thingToLog);
+void setPrintf(bool usePrintf) { USE_PRINTF = usePrintf;}
 #endif // LOGGING_BG
